a146f32db86251e5eec969b0944d117f6d5ab208
vaitheeswariv97 <v.vaitheeswari@kuwy.in>
2023-07-21 10:37:18 +0530

    push notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud_operation';
+  private readonly VAPID_PUBLIC_KEY =
+    'BBR6Wp_jdBtcamjbFKEzBOtxQlaWWTw2NqVDto9U-dY8FTg73kVFHsygy8x1_jxXfOOREX3qygVbqgA9398Su2c';
+
+  constructor(private swPush: SwPush) {}
+  ngOnInit(): void {
+    this.requestSubscription();
+  }
+
+  requestSubscription = () => {
+    if (!this.swPush.isEnabled) {
+      console.log('Notification is not enabled.');
+      return;
+    }
+    //console.log(this.VAPID_PUBLIC_KEY);
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY,
+      })
+      .then((_) => {
+        console.log(this.VAPID_PUBLIC_KEY);
+        console.log(JSON.stringify(_));
+      })
+      .catch((_) => console.log);
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,9 +10,10 @@ import { CrudShowComponent } from './crud-show/crud-show.component';
 import { CrudRemoveComponent } from './crud-remove/crud-remove.component';
 import { MessageComponent } from './message/message.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { ServiceWorkerModule } from '@angular/service-worker';
 const config: SocketIoConfig = {
-  url: 'http://192.168.34.75:5000',
-  //url: 'http://localhost:5000',
+  //url: 'http://192.168.34.75:5000',
+  url: 'http://localhost:5000',
   options: {},
 };
 
@@ -31,6 +32,12 @@ const config: SocketIoConfig = {
     ReactiveFormsModule,
     HttpClientModule,
     SocketIoModule.forRoot(config),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: true,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      //registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
